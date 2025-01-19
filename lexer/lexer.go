eddaa799a1bdb8646ba635fa03aac7200675ee87
Swaraj Buchude <swarajrb4@gmail.com>
2025-01-19 23:40:33 +0530

    handling of escape character in string

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -3,30 +3,30 @@ package lexer
 import (
 	"errors"
 	"fmt"
-	"unicode"
-	"regexp"
 	tok "github.com/swarajrb7/json-goparser/token"
 	"github.com/swarajrb7/json-goparser/utils"
+	"regexp"
+	"unicode"
 )
 
 var (
-	jsonTrue = []rune("true")
+	jsonTrue  = []rune("true")
 	jsonFalse = []rune("false")
-	jsonNull = []rune("null")
+	jsonNull  = []rune("null")
 )
 
-func Lexer(s string) ([]tok.Token, error) {	
+func Lexer(s string) ([]tok.Token, error) {
 	tokens := []tok.Token{}
 	lineNum := 1
 	colNum := 1
 
-	//rune is used to ensure that the string is a treated as a sequence of full Unicode characters, not just raw bytes.	
+	//rune is used to ensure that the string is a treated as a sequence of full Unicode characters, not just raw bytes.
 
 	for runes := []rune(s); len(runes) > 0; {
 		char := runes[0]
 		if unicode.IsSpace(char) {
 			colNum++
-			if char == '\n' {				
+			if char == '\n' {
 				lineNum++
 				colNum = 1
 			}
@@ -51,21 +51,21 @@ func Lexer(s string) ([]tok.Token, error) {
 			return []tok.Token{}, err
 		} else if token != (tok.Token{}) {
 			tokens = append(tokens, token)
-			colNum += len(token.Value) 
+			colNum += len(token.Value)
 			continue
 		}
 
 		token, runes, err = lexBool(runes, lineNum, colNum)
-		if err != nil {			
+		if err != nil {
 			return []tok.Token{}, err
 		} else if token != (tok.Token{}) {
 			tokens = append(tokens, token)
 			colNum += len(token.Value)
 			continue
-		}				
-		
+		}
+
 		token, runes, err = lexNull(runes, lineNum, colNum)
-		if err != nil {			
+		if err != nil {
 			return []tok.Token{}, err
 		} else if token != (tok.Token{}) {
 			tokens = append(tokens, token)
@@ -74,14 +74,14 @@ func Lexer(s string) ([]tok.Token, error) {
 		}
 		_, ok := tok.JsonSyntaxChars[char]
 		if ok {
-			tokens = append(tokens, tok.Token{tok.JsonSyntax ,string(char),lineNum, colNum})
+			tokens = append(tokens, tok.Token{tok.JsonSyntax, string(char), lineNum, colNum})
 			colNum++
 			runes = runes[1:]
-		} else {			
-			return []tok.Token{}, fmt.Errorf("lexer error: char %s at line %d col %d is invalid" , string(char), lineNum, colNum)
+		} else {
+			return []tok.Token{}, fmt.Errorf("lexer error: char %s at line %d col %d is invalid", string(char), lineNum, colNum)
 		}
 	}
-	return tokens,nil
+	return tokens, nil
 
 }
 
@@ -90,34 +90,44 @@ func lexString(runes []rune, lineNum int, colNum int) (tok.Token, []rune, error)
 		return tok.Token{}, runes, nil
 	}
 	rune := runes[1:]
+
+	escaped := false
+
 	for i, char := range rune {
-		if char == '"' {
+		if escaped {
+			switch char {
+			case 'b', 'f', 'n', 'r', 't', '"', '\\', '/':
+				escaped = false
+			default:
+				return tok.Token{tok.JsonString, string(runes[:i+1]), lineNum, colNum}, rune[i+1:], nil
+			}
+		} else if char == '\\' {
+			escaped = true
+		} else if char == '"' {
 			return tok.Token{tok.JsonString, string(runes[:i+1]), lineNum, colNum}, rune[i+1:], nil
 		}
 	}
-
 	return tok.Token{}, runes, errors.New("lexer error: EOF quote.")
 }
 
 func lexNumber(runes []rune, lineNum int, colNum int) (tok.Token, []rune, error) {
-	if !unicode.IsDigit(runes[0]) && runes[0] != '-'{
-		return tok.Token{}, runes, nil	
+	if !unicode.IsDigit(runes[0]) && runes[0] != '-' {
+		return tok.Token{}, runes, nil
 	}
 
 	var end int = len(runes)
 	for i, char := range runes {
-		if !unicode.IsDigit(char) && char != '.' && char != 'e' && char != 'E' && char != '-' {
-			end = i -1
+		if !unicode.IsDigit(char) && char != '.' && char != 'e' && char != 'E' && char != '-' && char != '+' {
+			end = i - 1
 			break
 		}
 	}
 
 	tokenValue := string(runes[:end+1])
-	if !regexp.MustCompile(`^\d+(?:\.\d+)?(?:e-?\d+)?$)`).MatchString(tokenValue) {
+	if !regexp.MustCompile(`^-?\d+(?:\.\d+)?(?:[eE][\-+]?\d+)?$`).MatchString(tokenValue) {
 		return tok.Token{}, runes, fmt.Errorf("lexer error: invalid number  %s", tokenValue)
 	}
 	return tok.Token{tok.JsonNumber, tokenValue, lineNum, colNum}, runes[end+1:], nil
-
 }
 
 func lexBool(runes []rune, lineNum int, colNum int) (tok.Token, []rune, error) {
@@ -137,36 +147,3 @@ func lexNull(runes []rune, lineNum int, colNum int) (tok.Token, []rune, error) {
 	}
 	return tok.Token{}, runes, nil
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
